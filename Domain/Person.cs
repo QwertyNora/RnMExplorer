af90b6be4c21138d3feb423986021ccdd59dd319
Nora Silfver <nora.silfver@gmail.com>
2025-10-20 11:10:37 +0200

    Add properties to Person class for character attributes

--- Domain/Person.cs
+++ Domain/Person.cs
@@ -3,5 +3,11 @@ namespace RnMExplorer.Domain;
 public sealed class Person
 {
     // TODO: Add properties
+    public string FirstName { get; }
+    public string LastName { get; }
+    public string? Species { get; }
+    public string? Status { get; }
+    public string? OriginName { get; }
+    public int EpisodeCount { get; }
     // TODO: Implement constructor
 }
