406873a3c8e4a20cf2a586b9373b3a741e90fce0
Nora Silfver <nora.silfver@gmail.com>
2025-10-20 10:12:07 +0200

    Add ConsoleUi class for handling console user interactions and displaying menus

---
+++ Ui/ConsoleUi.cs
@@ -0,0 +1,29 @@
+using RnmExplorer.Domain;
+
+namespace RnmExplorer.Ui;
+
+// Responsible for all console user interaction (printing menus and data).
+public sealed class ConsoleUi
+{
+    // TODO: Display a simple menu with user options.
+    public void PrintMenu()
+    {
+        Console.WriteLine("""
+        === Rick & Morty Explorer ===
+        1) Search by last name prefix
+        2) Show top-N characters (by episode count)
+        3) Group by species
+        4) Show simple projection (Name + Species + Episodes)
+        0) Exit
+        """);
+    }
+
+    // TODO: Print a list of Person objects in a table-like format.
+    // Example format:
+    // Rick Sanchez        | Human      | 51 episodes
+    public void PrintPeople(IEnumerable<Person> people)
+    {
+        // TODO: Loop through each person and print formatted data.
+        // Use string interpolation for nice alignment.
+    }
+}
